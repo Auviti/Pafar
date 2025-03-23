613718e9f0802c8058e6fd13ce3a71689d3e8216
oscaroguledo <oscarchiagoziem@gmail.com>
2025-03-23 03:21:36 +0000

    init frontend

--- frontend/pafar/src/App.jsx
+++ frontend/pafar/src/App.jsx
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState, useEffect, useMemo  } from 'react';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 import { WebSocketService } from './utils/webSockets';  // Import the WebSocketService
 import ThemeProvider from './utils/ThemeProvider'; // Import the ThemeProvider component
 
 
 function App() {
+  const [count, setCount] = useState(0)
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
 
@@ -32,18 +35,26 @@ function App() {
     <ThemeProvider>
         <div className="container">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
+          <div>
+              <a href="https://vite.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+              </a>
+              <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+              </p>
+            </div>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
             </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </header>
           <h1 className="text-center mt-5">React WebSocket App</h1>
           <div className="row mt-4">
@@ -85,3 +96,4 @@ function App() {
 }
 
 export default App;
+
--- frontend/pafar/src/utils/Themes.jsx
+++ frontend/pafar/src/utils/Themes.jsx
@@ -7,7 +7,7 @@ export const themes = {
     '--primary-color': '#007bff',
     '--button-bg': '#007bff',
     '--button-text': '#ffffff',
-    '--font-family': 'Roboto, sans-serif', // Light theme font
+    '--font-family': 'system-ui, Avenir, Helvetica, Roboto, sans-serif', // Light theme font
   },
   dark: {
     '--background-color': '#121212',
@@ -15,6 +15,6 @@ export const themes = {
     '--primary-color': '#1f8df3',
     '--button-bg': '#1f8df3',
     '--button-text': '#ffffff',
-    '--font-family': 'Roboto, sans-serif', // Dark theme font
+    '--font-family': 'system-ui, Avenir, Helvetica, Roboto, sans-serif', // Dark theme font
   },
 };
